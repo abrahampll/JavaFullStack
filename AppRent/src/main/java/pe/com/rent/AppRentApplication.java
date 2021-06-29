df0952512da52dc3094bf22b217f2d2cacd7e909
unknown <abraham20467k@gmail.com>
2021-06-29 15:16:42 -0500

    Create Proyect Backend

---
+++ AppRent/src/main/java/pe/com/rent/AppRentApplication.java
@@ -0,0 +1,13 @@
+package pe.com.rent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppRentApplication.class, args);
+	}
+
+}
